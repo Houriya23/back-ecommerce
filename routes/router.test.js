cc531f7d221eb6eed4ab20c0b566fc25d2d8e5d4
Mami Houria <houria.mami@enicar.ucar.tn>
2025-02-22 14:51:40 +0100

    generate report continue even though the test fail 2

--- routes/router.test.js
+++ routes/router.test.js
@@ -9,6 +9,6 @@ describe('Welcome Endpoint', () => {
   test('GET / should return "Welcome to the API !" message', async () => {
     const response = await request(app).get('/test');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ message: 'Welcome to the API !' });
+    expect(response.body).toEqual({ message: 'Welcome to the API fail please !' });
   });
 });
